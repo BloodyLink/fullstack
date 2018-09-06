7eff52500eeb208f31e4079fd0d160c653ec96f3
BloodyLink <david.quezada.m@gmail.com>
2018-09-06 02:58:07 -0300

    Categories are saved on db.

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -6,6 +6,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    url = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
--- techk/apps/scraper/views.py
+++ techk/apps/scraper/views.py
@@ -2,9 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from scraper.models import Category, Book
+from apps.scraper.models import Category, Book
 
-# from django.http import HttpResponse
+from django.http import HttpResponse
 import bs4 as bs
 import urllib.request
 
@@ -15,35 +15,49 @@ soup = bs.BeautifulSoup(sauce, "html.parser")
 #Obtenemos string con categorias desde sitio
 categoriesOrigin = soup.find("div", {"class": "side_categories"})
 #creamos un array con string de categorias
-categorias = categoriesOrigin.ul.li.text.split()
-#Eliminamos "Books" del arreglo.
-del(categorias[0])
+categorias = categoriesOrigin.ul.li.ul.text.split()
+# links = categoriesOrigin.ul.li.ul.find_all('a')
 
-cat = Category.objects.all()
+for cat in categoriesOrigin.ul.li.ul.find_all('a'):
+    # print(cat.get('href'))
+    # print(cat.text.strip())
+    c = Category(name=cat.text.strip(), url=cat.get('href'))
+    c.save(force_insert=True)
 
-print(cat)
+# print(links)
+# for cat in categorias:
+#     c = Category(name=cat)
+#     c.save(force_insert=True)
 
-booksOrigin = soup.findAll("article", {"class": "product_pod"})
-# print(len(booksOrigin))
-# print(booksOrigin[1].find("div", {"class": "product_price"}).find("p", {"class": "price_color"}).text)
-books = []
-
-class Book():
-    title = ""
-    price = ""
-
-    def __init__(self, title, price):
-        self.title = title
-        self.price = price
+def index(request):
+    return HttpResponse(links)
 
 
-for i in range(len(booksOrigin)):
-    title = booksOrigin[i].h3.a['title']
-    price = booksOrigin[i].find("div", {"class": "product_price"}).find("p", {"class": "price_color"}).text
+# cat = Category.objects.all()
+
+# print(cat)
+
+# booksOrigin = soup.findAll("article", {"class": "product_pod"})
+# # print(len(booksOrigin))
+# # print(booksOrigin[1].find("div", {"class": "product_price"}).find("p", {"class": "price_color"}).text)
+# books = []
+
+# class Book():
+#     title = ""
+#     price = ""
+
+#     def __init__(self, title, price):
+#         self.title = title
+#         self.price = price
+
+
+# for i in range(len(booksOrigin)):
+#     title = booksOrigin[i].h3.a['title']
+#     price = booksOrigin[i].find("div", {"class": "product_price"}).find("p", {"class": "price_color"}).text
     
-    book = Book(title, price)
-    # print(book.title)
-    books.append(book)
+#     book = Book(title, price)
+#     # print(book.title)
+#     books.append(book)
     
 # for b in books:
 #     print(b.price)
@@ -56,5 +70,4 @@ for i in range(len(booksOrigin)):
 
 # for book in booksOrigin:
 
-# def index(request):
-#     return HttpResponse(sopa.title)
+
