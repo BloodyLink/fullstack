572a75f80486e80fecbf232cabce450363e05ee5
BloodyLink <david.quezada.m@gmail.com>
2018-09-06 00:22:15 -0300

    Models and migrations done. Struggling with imports.

--- techk/apps/scraper/models.py
+++ techk/apps/scraper/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    thumbnail = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField();
+    description = models.TextField()
+    upc = models.CharField(max_length=50)
+    def __str__(self):
+        return self.title
--- techk/apps/scraper/views.py
+++ techk/apps/scraper/views.py
@@ -2,5 +2,59 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from scraper.models import Category, Book
 
-# Create your views here.
+# from django.http import HttpResponse
+import bs4 as bs
+import urllib.request
+
+
+sauce = urllib.request.urlopen('http://books.toscrape.com/').read()
+soup = bs.BeautifulSoup(sauce, "html.parser")
+
+#Obtenemos string con categorias desde sitio
+categoriesOrigin = soup.find("div", {"class": "side_categories"})
+#creamos un array con string de categorias
+categorias = categoriesOrigin.ul.li.text.split()
+#Eliminamos "Books" del arreglo.
+del(categorias[0])
+
+cat = Category.objects.all()
+
+print(cat)
+
+booksOrigin = soup.findAll("article", {"class": "product_pod"})
+# print(len(booksOrigin))
+# print(booksOrigin[1].find("div", {"class": "product_price"}).find("p", {"class": "price_color"}).text)
+books = []
+
+class Book():
+    title = ""
+    price = ""
+
+    def __init__(self, title, price):
+        self.title = title
+        self.price = price
+
+
+for i in range(len(booksOrigin)):
+    title = booksOrigin[i].h3.a['title']
+    price = booksOrigin[i].find("div", {"class": "product_price"}).find("p", {"class": "price_color"}).text
+    
+    book = Book(title, price)
+    # print(book.title)
+    books.append(book)
+    
+# for b in books:
+#     print(b.price)
+# print(books[1].price)
+
+# i = 0
+# while i < len(booksOrigin)
+#     if
+
+
+# for book in booksOrigin:
+
+# def index(request):
+#     return HttpResponse(sopa.title)
